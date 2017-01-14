2528c6316502dca2c3c519407c5304b43201f258
Estefania Morton <estefania.morton@gmail.com>
2017-01-14 10:41:23 -1000

    done

--- normal_cars.sql
+++ normal_cars.sql
@@ -9,25 +9,37 @@ CREATE DATABASE normal_cars WITH OWNER normal_user;
 \c normal_cars normal_user;
 \i scripts/denormal_data.sql;
 
-DROP TABLE IF EXISTS make_table;
-
 DROP TABLE IF EXISTS model_table;
 
-CREATE TABLE IF NOT EXISTS make_table (
-  id serial,
+DROP TABLE IF EXISTS make_table;
+
+
+CREATE TABLE make_table (
   make_code character varying(125),
-  make_title character varying(125)
+  make_title character varying(125),
+  id serial primary key
 );
 
-CREATE TABLE IF NOT EXISTS model_table (
-  id serial,
+CREATE TABLE model_table (
+  id serial primary key,
   model_code character varying(125) NOT NULL,
   model_title character varying(125) NOT NULL,
-  make_id integer,
-  year integer NOT NULL
+  year integer NOT NULL,
+  make_id integer references make_table(id)
 );
 
 INSERT into make_table (make_code, make_title) SELECT DISTINCT make_code, make_title FROM car_models;
 
-INSERT into model_table (model_code, model_title, year) SELECT DISTINCT model_code, model_title, year FROM car_models;
+INSERT INTO model_table (model_title, model_code, year, make_id)
+SELECT DISTINCT model_title, model_code, year, id AS make_id
+FROM car_models
+INNER JOIN make_table ON car_models.make_title = make_table.make_title;
+
+SELECT DISTINCT make_title FROM car_models;
+
+-- SELECT DISTINCT model_title FROM car_models WHERE make_code = 'VOLKS';
+
+-- SELECT DISTINCT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
+
+-- SELECT DISTINCT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
 
